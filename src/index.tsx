f6ba14aaea49b7e7c5c20f5bc277dac3433fe5a1
Randi Mays <randimays1024@gmail.com>
2021-12-14 08:15:59 -0600

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+    color?: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component<AppProps> {
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = {
+            counter: 0
+        };
+    }
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1})
+    }
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1})
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App color="red" />, document.getElementById('root'));
